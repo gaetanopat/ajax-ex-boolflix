389bf1a57f90cdcb64dde1be799ed1c0724ef30a
gaetanopat <patierno_gaetano@hotmail.com>
2019-05-29 14:01:55 +0200

    Fine Milestone 2

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
   // intercetto il click su 'Cerca'
   $('a.search').click(function(){
     cercaFilm(api_url_base);
+    cercaSerie(api_url_base);
+    // svuoto il div dei risultati della ricerca
+    $('.container_results').empty();
+    // 'azzero' il valore dell'input text
+    $('.input_search_movie').val('');
   });
 
   // intercetto il tasto invio
@@ -10,6 +15,11 @@ $(document).ready(function(){
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
       cercaFilm(api_url_base);
+      cercaSerie(api_url_base);
+      // svuoto il div dei risultati della ricerca
+      $('.container_results').empty();
+      // 'azzero' il valore dell'input text
+      $('.input_search_movie').val('');
     }
   });
 });
@@ -20,11 +30,6 @@ function cercaFilm(api){
   var info_film = $('#template_search_film').html();
   // funzione compilatrice
   var template_search_film_function = Handlebars.compile(info_film);
-
-  // codice da clonare
-  var bandierine = $('#template_bandiere').html();
-  // funzione compilatrice
-  var template_bandiere_function = Handlebars.compile(bandierine);
 
   // prendo il testo dalla casella di input dove l'utente scrive
   var film_cercato = $('.input_search_movie').val();
@@ -39,6 +44,7 @@ function cercaFilm(api){
         // language: 'it'
       },
       success: function (search_result) {
+        console.log('film');
         console.log(search_result);
         // controllo che ci siano risultati per il film che cerca l'utente
         if(search_result.results.length != 0){
@@ -48,6 +54,7 @@ function cercaFilm(api){
             handlebars_movie = {
               // Numero_risultato: (i+1) + '° Risultato',
               id: search_result.results[i].id,
+              type: 'film',
               title: search_result.results[i].title,
               original_title: search_result.results[i].original_title,
               language: search_result.results[i].original_language,
@@ -59,17 +66,7 @@ function cercaFilm(api){
 
             // funzione per le stelline
             visualizzaStelline(handlebars_movie.id, handlebars_movie.vote);
-
-            // per le bandiere, invece
-            // prendo l'h4.language del movie che sto ciclando
-            var lingua_movie = $('div.movie[data-id="' + handlebars_movie.id + '"] h4.language');
-            // variabile del template_bandiere
-            var handlebars_bandiere = {
-              bandiera: visualizzaBandiera(handlebars_movie.id, handlebars_movie.language)
-            }
-            var html2 = template_bandiere_function(handlebars_bandiere);
-            // appendo all0h4.language del template_search_film
-            lingua_movie.append(html2);
+            creaBandiera(handlebars_movie.id, handlebars_movie.language)
           }
         }else{
           // se l'utente ha inserito un film che non esiste
@@ -84,12 +81,61 @@ function cercaFilm(api){
     // se l'utente non ha scritto nulla nella casella input
     alert('Scrivi qualcosa');
   }
-  // svuoto il div dei risultati della ricerca
-  $('.container_results').empty();
-  // 'azzero' il valore dell'input text
-  $('.input_search_movie').val('');
+
 }
 
+
+function cercaSerie(api){
+  var info_film = $('#template_search_film').html();
+  // funzione compilatrice
+  var template_search_film_function = Handlebars.compile(info_film);
+
+  // prendo il testo dalla casella di input dove l'utente scrive
+  var film_cercato = $('.input_search_movie').val();
+  $.ajax({
+    url: api + '/search/tv',
+    method: 'get',
+    data: {
+      api_key: '1cd2b33115f891c68dafc6468ce95d73',
+      query: film_cercato,
+      // language: 'it'
+    },
+    success: function (search_result) {
+      console.log('serie');
+      console.log(search_result);
+      // controllo che ci siano risultati per il film che cerca l'utente
+      if(search_result.results.length != 0){
+        // oggetto che utilizzerò per popolare l'html
+        var handlebars_movie;
+        for (var i = 0; i < search_result.results.length; i++) {
+          handlebars_movie = {
+            // Numero_risultato: (i+1) + '° Risultato',
+            id: search_result.results[i].id,
+            type: 'serie tv',
+            title: search_result.results[i].name,
+            original_title: search_result.results[i].original_name,
+            language: search_result.results[i].original_language,
+            vote: search_result.results[i].vote_average,
+          }
+          var html = template_search_film_function(handlebars_movie);
+          // appendo al container
+          $('.container_results').append(html);
+
+          // funzione per le stelline
+          visualizzaStelline(handlebars_movie.id, handlebars_movie.vote);
+          creaBandiera(handlebars_movie.id, handlebars_movie.language)
+
+        }
+      }else{
+        // se l'utente ha inserito un film che non esiste
+        alert('Non esistono risultati per il film che hai cercato, riprova')
+      }
+    },
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. ");
+    }
+  });
+}
 // funzione per arrotondare il voto
 function arrotondaVoto(voto){
     var voto_arrotondato = Math.ceil(voto/2);
@@ -109,17 +155,34 @@ function visualizzaStelline(id, vote){
   });
 }
 
+function creaBandiera(id, lingua){
+  // codice da clonare
+  var bandierine = $('#template_bandiere').html();
+  // funzione compilatrice
+  var template_bandiere_function = Handlebars.compile(bandierine);
+
+  // per le bandiere, invece
+  // prendo l'h4.language del movie che sto ciclando
+  var lingua_movie = $('div.movie[data-id="' + id + '"] h4.language');
+  // per ognuno di questi
+  lingua_movie.each(function(){
+    // se la lingua è 'en', 'fr' o 'it' allora costruisco il template e lo appendo
+    if(lingua == 'en' || lingua == 'it' || lingua == 'fr'){
+      // variabile del template_bandiere
+      var handlebars_bandiere = {
+        bandiera: visualizzaBandiera(id, lingua)
+      }
+      var html2 = template_bandiere_function(handlebars_bandiere);
+      // appendo all0h4.language del template_search_film
+      lingua_movie.append(html2);
+    }
+  });
+}
 // funzione per visualizzare la bandiera corrispondente alla lingua
 function visualizzaBandiera(id, lingua){
   // prendo l'h4.language del movie che sto ciclando
   var lingua_movie = $('div.movie[data-id="' + id + '"] h4.language');
   switch(lingua){
-    case 'en':
-      // nascondo la lingua scritta in stringa
-      lingua_movie.children('span').hide();
-      // ritorno l'immagine
-      return 'https://cdn1.iconfinder.com/data/icons/world-flags-circular/1000/Flag_of_United_Kingdom_-_Circle-512.png';
-      break;
     case 'it':
       // nascondo la lingua scritta in stringa
       lingua_movie.children('span').hide();
@@ -132,7 +195,11 @@ function visualizzaBandiera(id, lingua){
       // ritorno l'immagine
       return 'https://cdn1.iconfinder.com/data/icons/european-country-flags/83/france-512.png';
       break;
+    // caso default, 'en'
     default:
-      return;
+      // nascondo la lingua scritta in stringa
+      lingua_movie.children('span').hide();
+      // ritorno l'immagine
+      return 'https://cdn1.iconfinder.com/data/icons/world-flags-circular/1000/Flag_of_United_Kingdom_-_Circle-512.png';
   }
 }
